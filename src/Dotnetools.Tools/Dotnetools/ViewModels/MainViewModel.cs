8dfca109160926da30329357a674ab3d9301c898
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-11-26 21:45:28 +0800

    使用ava默认的跨平台模板创建的初始化项目；

---
+++ src/Dotnetools.Tools/Dotnetools/ViewModels/MainViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Dotnetools.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ src/Dotnetools.Tools/Dotnetools/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Dotnetools.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
